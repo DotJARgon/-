e48720f4d2e4cfbb7c79327bda35351cd7bd29f6
unknown <mcar3550@students.kleinisd.net>
2023-11-21 16:37:24 -0600

    removed non null fields

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class User(db.Model):
         return self.username
 
 class People(db.Model):
-    playerId = db.Column(db.String(100), primary_key=True, nullable=False)
+    playerId = db.Column(db.String(100), primary_key=True)
     
     birthYear = db.Column(db.Integer())
     birthMonth = db.Column(db.Integer())
@@ -53,85 +53,82 @@ class People(db.Model):
     finalGameState = db.Column(db.String(100))
     finalGameCity = db.Column(db.String(100))
 
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255))
     weight = db.Column(db.Float())
     height = db.Column(db.Float())
     batHand = db.Column(db.Enum(Hand), default=Hand.N)
     throwHand = db.Column(db.Enum(Hand), default=Hand.N)
 
 class Parks(db.Model):
-    parkId = db.Column(db.String(32), primary_key=True, nullable=False)
-    parkName = db.Column(db.String(100), nullable=False)
+    parkId = db.Column(db.String(32), primary_key=True)
+    parkName = db.Column(db.String(100))
     parkAlias = db.Column(db.String(100))
-    parkCountry = db.Column(db.String(100), nullable=False)
-    parkState = db.Column(db.String(100), nullable=False)
-    parkCity = db.Column(db.String(100), nullable=False)
+    parkCountry = db.Column(db.String(100))
+    parkState = db.Column(db.String(100))
+    parkCity = db.Column(db.String(100))
 
 class Franchise(db.Model):
-    franchiseId = db.Column(db.String(32), primary_key=True, nullable=False)
-    franchiseName = db.Column(db.String(100), nullable=False)
-    active = db.Column(db.Enum(YNChoice), nullable=False, default=YNChoice.NA,)
+    franchiseId = db.Column(db.String(32), primary_key=True)
+    franchiseName = db.Column(db.String(100))
+    active = db.Column(db.Enum(YNChoice), default=YNChoice.NA,)
     nationalAssocId = db.Column(db.String(32))
 
-"""
-Made alot of assumptions on the nullable fields, so let us see
-"""
 class Team(db.Model):
-    teamId = db.Column(db.String(32), primary_key=True, nullable=False)
-    divId = db.Column(db.Enum(Division), nullable=False, default=Division.NONE)
-    leagueId = db.Column(db.String(32), nullable=False)
-    franchiseId = db.Column(db.ForeignKey('franchise.franchiseId'), nullable=False)
-    yearId = db.Column(db.Integer(), nullable=False)
+    teamId = db.Column(db.String(32), primary_key=True)
+    divId = db.Column(db.Enum(Division), default=Division.NONE)
+    leagueId = db.Column(db.String(32))
+    franchiseId = db.Column(db.ForeignKey('franchise.franchiseId'))
+    yearId = db.Column(db.Integer())
 
-    parkId = db.Column(db.ForeignKey('parks.parkId'), nullable=False)
+    parkId = db.Column(db.ForeignKey('parks.parkId'))
     attendance = db.Column(db.Integer()) # can be null just cause of lack of info :(
 
-    rank = db.Column(db.Integer(), nullable=False)
-    gamesPlayed = db.Column(db.Integer(), nullable=False, default=0)
-    homeGamesPlayed = db.Column(db.Integer(), nullable=False, default=0)
-    wins = db.Column(db.Integer(), nullable=False, default=0)
-    losses = db.Column(db.Integer(), nullable=False, default=0)
+    rank = db.Column(db.Integer())
+    gamesPlayed = db.Column(db.Integer(), default=0)
+    homeGamesPlayed = db.Column(db.Integer(), default=0)
+    wins = db.Column(db.Integer(), default=0)
+    losses = db.Column(db.Integer(), default=0)
 
     """
     I chose to use the YNChoice enum so we do not have null fields, trying to reduce those to
     a minimum
     """
-    divWon = db.Column(db.Enum(YNChoice), nullable=False, default=YNChoice.NA)
-    worldCupWon = db.Column(db.Enum(YNChoice), nullable=False, default=YNChoice.NA)
-    leagueWon = db.Column(db.Enum(YNChoice), nullable=False, default=YNChoice.NA)
-    worldSeriesWon = db.Column(db.Enum(YNChoice), nullable=False, default=YNChoice.NA)
+    divWon = db.Column(db.Enum(YNChoice), default=YNChoice.NA)
+    worldCupWon = db.Column(db.Enum(YNChoice), default=YNChoice.NA)
+    leagueWon = db.Column(db.Enum(YNChoice), default=YNChoice.NA)
+    worldSeriesWon = db.Column(db.Enum(YNChoice), default=YNChoice.NA)
 
-    runs = db.Column(db.Integer(), nullable=False, default=0)
-    atBats = db.Column(db.Integer(), nullable=False, default=0)
+    runs = db.Column(db.Integer(), default=0)
+    atBats = db.Column(db.Integer(), default=0)
 
-    batterHit = db.Column(db.Integer(), nullable=False, default=0)
-    batterHomeRuns = db.Column(db.Integer(), nullable=False, default=0)
-    batterWalks = db.Column(db.Integer(), nullable=False, default=0)
+    batterHit = db.Column(db.Integer(), default=0)
+    batterHomeRuns = db.Column(db.Integer(), default=0)
+    batterWalks = db.Column(db.Integer(), default=0)
 
-    doubles = db.Column(db.Integer(), nullable=False, default=0)
-    triples = db.Column(db.Integer(), nullable=False, default=0)
-    strikeouts = db.Column(db.Integer(), nullable=False, default=0)
-    stolenBases = db.Column(db.Integer(), nullable=False, default=0)
-    caughtStealing = db.Column(db.Integer(), nullable=False, default=0)
-    batterHitPitch = db.Column(db.Integer(), nullable=False, default=0)
-    sacrificeFlies = db.Column(db.Integer(), nullable=False, default=0)
-    opponentRunsScored = db.Column(db.Integer(), nullable=False, default=0)
-    earnedRunsAllowed = db.Column(db.Integer(), nullable=False, default=0)
-    earnedRunAverage = db.Column(db.Float(), nullable=False, default=0.0) #idk if this should be here tbh
-    completeGames = db.Column(db.Integer(), nullable=False, default=0)
-    shutouts = db.Column(db.Integer(), nullable=False, default=0)
-    saves = db.Column(db.Integer(), nullable=False, default=0)
-    ipOuts = db.Column(db.Integer(), nullable=False, default=0)
-    hitsAllowed = db.Column(db.Integer(), nullable=False, default=0)
-    homerunsAllowed = db.Column(db.Integer(), nullable=False, default=0)
-    walksAllowed = db.Column(db.Integer(), nullable=False, default=0)
-    pitcherStrikeouts = db.Column(db.Integer(), nullable=False, default=0)
-    errors = db.Column(db.Integer(), nullable=False, default=0)
-    doublePlays = db.Column(db.Integer(), nullable=False, default=0)
-    fieldPercentage = db.Column(db.Float(), nullable=False, default=0.0)
+    doubles = db.Column(db.Integer(), default=0)
+    triples = db.Column(db.Integer(), default=0)
+    strikeouts = db.Column(db.Integer(), default=0)
+    stolenBases = db.Column(db.Integer(), default=0)
+    caughtStealing = db.Column(db.Integer(), default=0)
+    batterHitPitch = db.Column(db.Integer(), default=0)
+    sacrificeFlies = db.Column(db.Integer(), default=0)
+    opponentRunsScored = db.Column(db.Integer(), default=0)
+    earnedRunsAllowed = db.Column(db.Integer(), default=0)
+    earnedRunAverage = db.Column(db.Float(), default=0.0) #idk if this should be here tbh
+    completeGames = db.Column(db.Integer(), default=0)
+    shutouts = db.Column(db.Integer(), default=0)
+    saves = db.Column(db.Integer(), default=0)
+    ipOuts = db.Column(db.Integer(), default=0)
+    hitsAllowed = db.Column(db.Integer(), default=0)
+    homerunsAllowed = db.Column(db.Integer(), default=0)
+    walksAllowed = db.Column(db.Integer(), default=0)
+    pitcherStrikeouts = db.Column(db.Integer(), default=0)
+    errors = db.Column(db.Integer(), default=0)
+    doublePlays = db.Column(db.Integer(), default=0)
+    fieldPercentage = db.Column(db.Float(), default=0.0)
 
-    batterParkFactor = db.Column(db.Integer(), nullable=False, default=0)
-    pitcherParkFactor = db.Column(db.Integer(), nullable=False, default=0)
+    batterParkFactor = db.Column(db.Integer(), default=0)
+    pitcherParkFactor = db.Column(db.Integer(), default=0)
 
 
 
